1185d3074f5cf42aa8d89f7f7835241cd7693e72
Christopher Thielen <cmthielen@ucdavis.edu>
2017-01-26 15:07:38 -0800

    Convert credentials to read from S3 instead of file.

--- README.md
+++ README.md
@@ -7,17 +7,17 @@ the UC Davis organization.
 Setting Up with AWS Lambda, AWS KMS, and AWS Gateway API
 --------------------------------------------------------
 
-1. Set up the KMS-secured credentials file:
+1. Set up the KMS-encrypted credentials file in S3:
 
-	The script currently stores credentials for GitHub in a file named 'encrypted-secret', meant to be in the same directory as the script.
+	Credentials for GitHub are a JSON file encrypted by KMS and stored in a S3 bucket.
 
-	The file is encrypted using a key stored in AWS' KMS. Once you have an encryption key in KMS, create the file:
+	To produce the file:
 
-	(Using the 'aws' CLI)
+	aws kms encrypt --key-id THE_AWS_KMS_KEY_ID --plaintext "{\"username\": \"ORG_WRITING_GH_USERNAME\", \"password\": \"THE_PASSWORD\", \"token\": \"TOKEN_TO_RUN\"}" --query CiphertextBlob --output text | base64 -D > ./encrypted-secret
 
-	aws kms encrypt --key-id THE_KEY_ID --plaintext "{\"username\": \"ORG_WRITING_GH_USERNAME\", \"password\": \"THE_PASSWORD\", \"token\": \"TOKEN_TO_RUN\"}" --query CiphertextBlob --output text | base64 -D > ./encrypted-secret
+	Copy the resulting file to S3 and configure the bucket name and key in the constants section near the top of index.js.
 
-2. Package the code with the encrypted file and upload to Lambda:
+2. Upload the code to Lambda:
 
 	To upload to AWS Lambda, select their upload function and provide it with a .zip file generated, e.g.:
 
@@ -27,7 +27,7 @@ Setting Up with AWS Lambda, AWS KMS, and AWS Gateway API
 Using
 -----
 
-Once set up, run, e.g. curl -v "https://something.amazonaws.com/stag_name/lambda_name?username=EXISTING_GH_USERNAME".
+Once set up, run, e.g. curl -v "https://something.amazonaws.com/stag_name/lambda_name?username=EXISTING_GH_USERNAME&token=TOKEN_TO_RUN".
 
 Version 1.0
 Author: Christopher Thielen <cmthielen@(at)ucdavis.edu>
--- index.js
+++ index.js
@@ -2,13 +2,17 @@
 
 // Required packages provided by AWS Lambda
 var fs = require('fs');
-var AWS = require('aws-sdk');
+const AWS = require('aws-sdk');
+const S3 = new AWS.S3({ apiVersion: '2006-03-01' });
 // Required packages found in ./node_modules/
 var GitHubApi = require("github");
 
-const GITHUB_API_TIMEOUT = 5000;                 // GitHub API timeout
-const AWS_REGION = 'us-west-2';                  // required to use AWS KMS service
-const ENCRYPTED_FILENAME = './encrypted-secret'; // filename of encrypted KMS credentials
+const GITHUB_API_TIMEOUT = 5000;
+// AWS KMS service is region-specific
+const AWS_REGION         = 'us-west-2';
+// S3 bucket/key with encrpyted GitHub credentials and usage token
+const S3_BUCKET_NAME     = 'lambda-github-credentials';
+const S3_KEY_NAME        = 'encrypted-secret';
 
 // Check if environment supports native promises
 if(typeof Promise === 'undefined') {
@@ -35,89 +39,107 @@ exports.handler = (event, context, callback) => {
 
     // Decrypt token and GitHub credentials using key stored in AWS' KMS
     var kms = new AWS.KMS({region: AWS_REGION});
-    var encryptedSecret = fs.readFileSync(ENCRYPTED_FILENAME);
 
-    kms.decrypt({ CiphertextBlob: encryptedSecret }, function(err, data) {
+    S3.getObject({
+        Bucket: S3_BUCKET_NAME,
+        Key: S3_KEY_NAME,
+    }, function(err, data) {
         if(err != null) {
-            console.log("Unable to decrpyt from KMS.");
-            console.log(err, err.stack);
-            
             callback(null, {
                 "statusCode": 500,
                 "headers": {},
                 "body": JSON.stringify({
                     success: false,
                     username: null,
-                    message: "Error while retrieving GitHub credentials."
+                    message: "Unable to retrieve credentials from S3"
                 })
             });
         } else {
-            var decryptedScret = data['Plaintext'].toString();
-            var credentials = JSON.parse(decryptedScret);
+            let encryptedS3data = data.Body;
 
-            if(event.queryStringParameters.token != credentials.token) {
-                // Supplied username and password but token is incorrect
-                callback(null, {
-                    "statusCode": 401,
-                    "headers": {},
-                    "body": JSON.stringify({
-                        success: false,
-                        username: null,
-                        message: "GET parameter 'token' is incorrect."
-                    })
-                });
-            } else {
-                // We have the username and the correct token, now try to use the GitHub API ...
-                var github = new GitHubApi({
-                    debug: true,
-                    protocol: "https",
-                    host: "api.github.com",
-                    pathPrefix: "",
-                    headers: {
-                        "user-agent": "UCD-DSS-GH-Provision"
-                    },
-                    Promise: require('bluebird'),
-                    followRedirects: false,
-                    timeout: GITHUB_API_TIMEOUT
-                });
+            kms.decrypt({ CiphertextBlob: encryptedS3data }, function(err, data) {
+                if(err != null) {
+                    console.log("Unable to decrpyt from KMS.");
+                    console.log(err, err.stack);
+                    
+                    callback(null, {
+                        "statusCode": 500,
+                        "headers": {},
+                        "body": JSON.stringify({
+                            success: false,
+                            username: null,
+                            message: "Error while decrypting GitHub credentials."
+                        })
+                    });
+                } else {
+                    var decryptedScret = data['Plaintext'].toString();
+                    var credentials = JSON.parse(decryptedScret);
 
-                github.authenticate({
-                    type: "basic",
-                    username: credentials.username,
-                    password: credentials.password
-                });
-
-                // Add member to org
-                github.orgs.addOrgMembership({
-                    org: "ucdavis",
-                    username: event.queryStringParameters.username,
-                    role: "member"
-                }, function (err, res) {
-                    if(err == null) {
-                        // Member added. Indicate success to Lambda!
+                    if(event.queryStringParameters.token != credentials.token) {
+                        // Supplied username and password but token is incorrect
                         callback(null, {
-                            "statusCode": 200,
-                            "headers": {},
-                            "body": JSON.stringify({
-                                success: true,
-                                username: event.queryStringParameters.username,
-                                message: "User added or is already in organization."
-                            })
-                        });
-                    } else {
-                        // Error while communicating with GitHub, indicate failure to Lambda.
-                        callback(null, {
-                            "statusCode": 500,
+                            "statusCode": 401,
                             "headers": {},
                             "body": JSON.stringify({
                                 success: false,
-                                username: event.queryStringParameters.username,
-                                message: "GitHub API returned an error."
+                                username: null,
+                                message: "GET parameter 'token' is incorrect."
                             })
                         });
+                    } else {
+                        // We have the username and the correct token, now try to use the GitHub API ...
+                        var github = new GitHubApi({
+                            debug: true,
+                            protocol: "https",
+                            host: "api.github.com",
+                            pathPrefix: "",
+                            headers: {
+                                "user-agent": "UCD-DSS-GH-Provision"
+                            },
+                            Promise: require('bluebird'),
+                            followRedirects: false,
+                            timeout: GITHUB_API_TIMEOUT
+                        });
+
+                        github.authenticate({
+                            type: "basic",
+                            username: credentials.username,
+                            password: credentials.password
+                        });
+
+                        // Add member to org
+                        github.orgs.addOrgMembership({
+                            org: "ucdavis",
+                            username: event.queryStringParameters.username,
+                            role: "member"
+                        }, function (err, res) {
+                            if(err == null) {
+                                // Member added. Indicate success to Lambda!
+                                callback(null, {
+                                    "statusCode": 200,
+                                    "headers": {},
+                                    "body": JSON.stringify({
+                                        success: true,
+                                        username: event.queryStringParameters.username,
+                                        message: "User added or is already in organization."
+                                    })
+                                });
+                            } else {
+                                // Error while communicating with GitHub, indicate failure to Lambda.
+                                callback(null, {
+                                    "statusCode": 500,
+                                    "headers": {},
+                                    "body": JSON.stringify({
+                                        success: false,
+                                        username: event.queryStringParameters.username,
+                                        message: "GitHub API returned an error."
+                                    })
+                                });
+                            }
+                        });
                     }
-                });
-            }
+                }
+            });
         }
     });
 };
