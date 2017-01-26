e80c04283d6a6b370103343bcf4f003dbc6e70cb
Christopher Thielen <cmthielen@ucdavis.edu>
2017-01-26 14:36:56 -0800

    Cleaned up index.js

--- index.js
+++ index.js
@@ -1,36 +1,73 @@
 'use strict';
 
+// Required packages provided by AWS Lambda
+var fs = require('fs');
+var AWS = require('aws-sdk');
+// Required packages found in ./node_modules/
+var GitHubApi = require("github");
+
+const GITHUB_API_TIMEOUT = 5000;                 // GitHub API timeout
+const AWS_REGION = 'us-west-2';                  // required to use AWS KMS service
+const ENCRYPTED_FILENAME = './encrypted-secret'; // filename of encrypted KMS credentials
+
+// Check if environment supports native promises
+if(typeof Promise === 'undefined') {
+    AWS.config.setPromisesDependency(require('bluebird'));
+}
+
 // Export the AWS Lambda handler as required.
 exports.handler = (event, context, callback) => {
-    var data = null;
+    // Ensure we received the 'username' and 'token' GET parameters
+    if((event.queryStringParameters === undefined) || (event.queryStringParameters.username === undefined) || (event.queryStringParameters.token === undefined)) {
+        // Incorrect GET parameters
+        callback(null, {
+            "statusCode": 400,
+            "headers": {},
+            "body": JSON.stringify({
+                success: false,
+                username: null,
+                message: "GET parameter 'username' and/or 'token' is missing."
+            })
+        });
 
-    // Ensure we received the 'username' GET parameter
-    if((event.queryStringParameters !== undefined) && (event.queryStringParameters.username !== undefined)) {
-        // First, decrypt GitHub credentials using key stored in AWS' KMS
-        var fs = require('fs');
-        var AWS = require('aws-sdk');
-        var kms = new AWS.KMS({region:'us-west-2'});
+        return;
+    }
 
-        var secretPath = './encrypted-secret';
-        var encryptedSecret = fs.readFileSync(secretPath);
+    // Decrypt token and GitHub credentials using key stored in AWS' KMS
+    var kms = new AWS.KMS({region: AWS_REGION});
+    var encryptedSecret = fs.readFileSync(ENCRYPTED_FILENAME);
 
-        var params = {
-            CiphertextBlob: encryptedSecret
-        };
+    kms.decrypt({ CiphertextBlob: encryptedSecret }, function(err, data) {
+        if(err != null) {
+            console.log("Unable to decrpyt from KMS.");
+            console.log(err, err.stack);
+            
+            callback(null, {
+                "statusCode": 500,
+                "headers": {},
+                "body": JSON.stringify({
+                    success: false,
+                    username: null,
+                    message: "Error while retrieving GitHub credentials."
+                })
+            });
+        } else {
+            var decryptedScret = data['Plaintext'].toString();
+            var credentials = JSON.parse(decryptedScret);
 
-        // Attempt to decrypt ...
-        kms.decrypt(params, function(err, data) {
-            if(err) {
-                console.log("Unable to decrpyt from KMS.");
-                console.log(err, err.stack);
+            if(event.queryStringParameters.token != credentials.token) {
+                // Supplied username and password but token is incorrect
+                callback(null, {
+                    "statusCode": 401,
+                    "headers": {},
+                    "body": JSON.stringify({
+                        success: false,
+                        username: null,
+                        message: "GET parameter 'token' is incorrect."
+                    })
+                });
             } else {
-                // Success. Now try to use the GitHub API ...
-                var decryptedScret = data['Plaintext'].toString();
-
-                var credentials = JSON.parse(decryptedScret);
-
-                var GitHubApi = require("github");
-
+                // We have the username and the correct token, now try to use the GitHub API ...
                 var github = new GitHubApi({
                     debug: true,
                     protocol: "https",
@@ -40,8 +77,8 @@ exports.handler = (event, context, callback) => {
                         "user-agent": "UCD-DSS-GH-Provision"
                     },
                     Promise: require('bluebird'),
-                    followRedirects: false, // default: true; there's currently an issue with non-get redirects, so allow ability to disable follow-redirects
-                    timeout: 5000
+                    followRedirects: false,
+                    timeout: GITHUB_API_TIMEOUT
                 });
 
                 github.authenticate({
@@ -58,50 +95,29 @@ exports.handler = (event, context, callback) => {
                 }, function (err, res) {
                     if(err == null) {
                         // Member added. Indicate success to Lambda!
-                        data = {
-                            success: true,
-                            username: event.queryStringParameters.username
-                        };
-                    
-                        // here's the object we need to return
-                        const res = {
+                        callback(null, {
                             "statusCode": 200,
                             "headers": {},
-                            "body": JSON.stringify(data)
-                        };
-                        
-                        callback(null, res);
+                            "body": JSON.stringify({
+                                success: true,
+                                username: event.queryStringParameters.username,
+                                message: "User added or is already in organization."
+                            })
+                        });
                     } else {
                         // Error while communicating with GitHub, indicate failure to Lambda.
-                        data = {
-                            success: false,
-                            username: null
-                        };
-
-                        const res = {
+                        callback(null, {
                             "statusCode": 500,
                             "headers": {},
-                            "body": JSON.stringify(data)
-                        };
-                    
-                        callback(null, res);
+                            "body": JSON.stringify({
+                                success: false,
+                                username: event.queryStringParameters.username,
+                                message: "GitHub API returned an error."
+                            })
+                        });
                     }
                 });
             }
-        });
-    } else {
-        // Incorrect GET parameters
-        data = {
-           success: false,
-           username: null
-        };
-
-        const res = {
-            "statusCode": 400,
-            "headers": {},
-            "body": JSON.stringify(data)
-        };
-    
-        callback(null, res);
-    }
+        }
+    });
 };
