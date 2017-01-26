e595987991b7523dda5489820488915825482d42
Christopher Thielen <cmthielen@ucdavis.edu>
2017-01-26 12:37:02 -0800

    Added the most basic of comments.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 'use strict';
 
+// Export the AWS Lambda handler as required.
 exports.handler = (event, context, callback) => {
     var data = null;
 
+    // Ensure we received the 'username' GET parameter
     if((event.queryStringParameters !== undefined) && (event.queryStringParameters.username !== undefined)) {
-        // Decrypt GitHub credentials using key stored in AWS' KMS
+        // First, decrypt GitHub credentials using key stored in AWS' KMS
         var fs = require('fs');
         var AWS = require('aws-sdk');
         var kms = new AWS.KMS({region:'us-west-2'});
@@ -16,20 +18,20 @@ exports.handler = (event, context, callback) => {
             CiphertextBlob: encryptedSecret
         };
 
+        // Attempt to decrypt ...
         kms.decrypt(params, function(err, data) {
             if(err) {
                 console.log("Unable to decrpyt from KMS.");
                 console.log(err, err.stack);
             } else {
+                // Success. Now try to use the GitHub API ...
                 var decryptedScret = data['Plaintext'].toString();
 
                 var credentials = JSON.parse(decryptedScret);
 
-                // Key obtained and decrypted. Now try GitHub ...
                 var GitHubApi = require("github");
 
                 var github = new GitHubApi({
-                    // optional
                     debug: true,
                     protocol: "https",
                     host: "api.github.com",
@@ -55,7 +57,7 @@ exports.handler = (event, context, callback) => {
                     role: "member"
                 }, function (err, res) {
                     if(err == null) {
-                        // Username supplied. Attempt to add to GitHub
+                        // Member added. Indicate success to Lambda!
                         data = {
                             success: true,
                             username: event.queryStringParameters.username
@@ -70,7 +72,7 @@ exports.handler = (event, context, callback) => {
                         
                         callback(null, res);
                     } else {
-                        // Error while communicating with GitHub
+                        // Error while communicating with GitHub, indicate failure to Lambda.
                         data = {
                             success: false,
                             username: null
@@ -88,7 +90,7 @@ exports.handler = (event, context, callback) => {
             }
         });
     } else {
-        // Incorrect query parameters
+        // Incorrect GET parameters
         data = {
            success: false,
            username: null
